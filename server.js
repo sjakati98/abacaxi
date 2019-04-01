67d1e87a8ee2499241b28d7e2c3f394331c1b26b
Sean Kelley <seangtkelley@gmail.com>
2019-04-01 16:44:59 -0400

    updated server.js to use mongo

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
 
 const app = express();
+const bodyParser = require('body-parser');
 
 app.use(express.static('static'));
+app.use(bodyParser.json());
 
-app.listen(3000, function () {
+const MongoClient = require('mongodb').MongoClient;
+
+let db;
+MongoClient.connect('mongodb://localhost', { useNewUrlParser: true }).then(connection => {
+  db = connection.db('abacaxi');
+  app.listen(3000, () => {
     console.log('App started on port 3000');
+  });
+}).catch(error => {
+  console.log('ERROR:', error);
 });
