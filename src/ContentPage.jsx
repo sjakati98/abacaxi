ba8573ded65c1960b642d4bd978a81762efe3c3d
YuanxunTao <yuanxuntao@umass.edu>
2019-04-07 15:58:32 -0400

    Update ContentPage.jsx

--- src/ContentPage.jsx
+++ src/ContentPage.jsx
@@ -270,6 +270,28 @@ const Video = (props) => (
   </div>
 );
 
+const AddVideoModal = (props) =>(
+  <div className="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div className="modal-dialog modal-dialog-centered" role="document">
+      <div className="modal-content">
+        <div className="modal-header">
+          <h5 className="modal-title" id="exampleModalLongTitle">Modal title</h5>
+          <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div className="modal-body">
+          blablablablablablablablabla
+        </div>
+        <div className="modal-footer">
+          <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="button" className="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
+)
+
 function Section(props) {
   const CustomHeader = `h${props.section.level}`;
   const videos = example_videos.map(video => (
@@ -307,6 +329,10 @@ class ContentPage extends React.Component {
     return (
       <div className="container">
         <h1>Content Page</h1>
+        <hr />
+        <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+          Add Video
+        </button>
         <hr />
         <WikiPage sections={this.state.sections} />
       </div>
